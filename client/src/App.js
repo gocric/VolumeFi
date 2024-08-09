b8372438688b8fb68abbe95811d4d8a27a2b8e49
Gokul Vijayasarathy <gokul.vijayasarathy@gmail.com>
2024-08-08 18:32:47 -0700

    Updating command to remove flights

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
+let cors = require('cors')
 
 function App() {
 
@@ -10,7 +11,7 @@ function App() {
   let [origin, setOrigin]=useState('')
   let [final_destination, setFinalDestination] = useState('')
   let [output, setOutput]=useState('')
-  
+
   function handleAddInputs(event){
     if (event.key === 'Enter') {
       setFlights([...flights,...[[origin, final_destination]]])
@@ -18,23 +19,34 @@ function App() {
       setFinalDestination('')
     }
   }
+
   function calculate(){
-    axios.post('http://localhost:8080').then((data) => {
-      //this console.log will be in our frontend console
+    setOutput('')
+    axios.post('http://localhost:8080/calculate', flights).then((data) => {
       console.log(data)
+      setOutput(data.data)
+    }).catch((data)=>{
+      setOutput([data.response.data,""])
     })
   }
 
+
+  function removeFlight(e) {
+    setFlights(flights.filter(function(flight) { 
+        return flight !== e 
+  }));
+}
   return (
     <div className="App"><br/>
         From : <input value={origin} onChange={(e)=>setOrigin(e.target.value)}/><br/>
         To : <input  value={final_destination} onChange={(e)=>setFinalDestination(e.target.value)} onKeyDown={handleAddInputs} /> 
         {flights.map(function(item){
-          return (<div>{item[0]}-{item[1]}<a style={{"paddingLeft":"10px", "cursor":"pointer"}}>X</a></div>)
+          return (<div>{item[0]}-{item[1]}<a style={{"paddingLeft":"10px", "cursor":"pointer"}} onClick={(e)=>removeFlight(item)}>X</a></div>)
         })}
+        <br/>
         <button onClick={calculate}>Calculate</button>
+        <div>{output!==''?`${output[0]},${output[1]}`:null}</div>
     </div>
   );
 }
-
 export default App;
--- index.js
+++ index.js
@@ -1,30 +1,44 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+let cors = require('cors')
 
-app.get('/calculate', (req, res) => {
-  const flights =[['IND', 'EWR'], ['SFO', 'ATL'], ['GSO', 'IND']];
+var corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+app.use(express.json()); // Middleware to parse JSON request bodies
+
+
+app.post('/calculate', (req, res) => {
+ console.log(req.body)
+  const flights = req.body;
   let from_destinations = new Set();
   let to_destinations = new Set();
   for (const [from_destination, to_destination] of flights) {
     from_destinations.add(from_destination)
     to_destinations.add(to_destination)
   }
-  console.log(from_destinations)
+
   let all_destinations = new Set([...from_destinations, ...to_destinations]);
   from_destinations =[...from_destinations]
   to_destinations = [...to_destinations]
   let origin = [...all_destinations].filter(x => !to_destinations.includes(x))
   let final_destination = [...all_destinations].filter(x => !from_destinations.includes(x))
-  if (origin.length==1 && final_destination.at.length==1)
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");  
+  console.log(origin)
+  console.log(final_destination)
+  if (origin.length!=1 || final_destination.length!=1)
   {
-    res.json([origin,final_destination]);
+    res.status(500).json("Cannot find origin and destination based on the given information")
+
   }
   else{
-    res.status(500).json("Cannot find origin and destination based on the given information")
+    res.json([origin[0],final_destination[0]]);
   }
 });
-
+app.use(cors(corsOptions))
 app.listen(port, () => {
   console.log(`Microservice listening on port ${port}`);
 });
